b2da4062c4c9e327422c16cd4848a05dde3a863e
dirkgroenendijk1 <dirkgroenendijk1@gmail.com>
2021-12-09 12:01:36 +0100

    activeren fimzoeker functie

--- eindopdracht-filmzoeker/script.js
+++ eindopdracht-filmzoeker/script.js
@@ -22,3 +22,62 @@ function addMoviesToDom(movies) {
         movieNav.appendChild(movieList);
     });
 };
+
+// verwijderen vam movies van de dom
+const removeMovieFromDom = () => {
+    const movies = movieNav.childNodes.length;
+    for (let i = 0; i < movies; i++) {
+        movieNav.removeChild(movieNav.childNodes[0]);
+    }
+};
+
+// oppakken van de buttons 
+function addEventListener() {
+    const filmfilter = document.querySelectorAll('input[type=radio][name="movie"]');
+    filmfilter.forEach(radio => radio.addEventListener('change', () => {
+        console.log("de " + radio.value + " button is ingedrukt")
+        handleOnChangeEvent(radio.value)
+    }));
+}
+addEventListener()
+//fiteren van nieuwe films 
+const newMovie = movies.filter((movie) => movie.Year >= 2014);
+
+
+// filteren van filmtype
+const filterMovies = (nameInMovieTitle) => {
+    const filterMovies = movies.filter((movie) => movie.Title.includes(nameInMovieTitle));
+    console.log(filterMovies)
+    return filterMovies;
+};
+
+// het activeren van de filter funtie
+function handleOnChangeEvent(event) {
+    removeMovieFromDom();
+    switch (event) {
+        case 'newmovies':
+            console.log("newmovie");
+            addMoviesToDom(newMovie);
+            break;
+        case 'avenger':
+            console.log("avenger");
+            addMoviesToDom(filterMovies("Avenger"));
+            break;
+        case 'xmen':
+            console.log("X-men");
+            addMoviesToDom(filterMovies("X-Men"));
+            break;
+        case 'princess':
+             console.log("princess");
+            addMoviesToDom(filterMovies("Princess"));
+            break;
+        case 'batman':
+            console.log("batman");
+            addMoviesToDom(filterMovies("Batman"));
+            break;
+        default:
+            addMoviesToDom(movies)
+    }
+
+}
+handleOnChangeEvent();
