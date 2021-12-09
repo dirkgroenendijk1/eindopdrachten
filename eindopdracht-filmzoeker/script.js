2db02d5af7e8b6ca84f790741495fe38dc594249
dirkgroenendijk1 <dirkgroenendijk1@gmail.com>
2021-12-09 11:32:57 +0100

    filterfunctie

--- eindopdracht-filmzoeker/script.js
+++ eindopdracht-filmzoeker/script.js
@@ -22,3 +22,32 @@ function addMoviesToDom(movies) {
         movieNav.appendChild(movieList);
     });
 };
+
+// verwijderen vam movies van de dom
+const removeMovieFromDom = () => {
+    const movies = movieNav.childNodes.length;
+    for (let i = 0; i < movies; i++) {
+        movieNav.removeChild(movieNav.childNodes[0]);
+    }
+};
+
+// oppakken van de buttons 
+function addEventListener() {
+    const filmfilter = document.querySelectorAll('input[type=radio][name="movie"]');
+    filmfilter.forEach(radio => radio.addEventListener('change', () => {
+        console.log("de " + radio.value + " button is ingedrukt")
+        handleOnChangeEvent(radio.value)
+    }));
+}
+addEventListener()
+
+//fiteren van nieuwe films 
+const newMovie = movies.filter((movie) => movie.Year >= 2014);
+
+
+// filteren van filmtype
+const filterMovies = (nameInMovieTitle) => {
+    const filterMovies = movies.filter((movie) => movie.Title.includes(nameInMovieTitle));
+    console.log(filterMovies)
+    return filterMovies;
+};
