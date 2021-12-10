ac520294b9a6502b5b9d686fa0216a994fdfe988
dirkgroenendijk1 <dirkgroenendijk1@gmail.com>
2021-12-10 11:56:39 +0100

    update na verslag

--- eindopdracht-filmzoeker/script.js
+++ eindopdracht-filmzoeker/script.js
@@ -1,8 +1,8 @@
 const movieNav = document.getElementById('movie-container');
 const searchBar = document.forms['search-movies'].querySelector('input');
-// inladen movies van data op de dom
+
 function addMoviesToDom(movies) {
-    var movieList = movies.map((movie => {
+    const movieList = movies.map((movie => {
         let movieList = document.createElement('li');
         //console.log(movieList);
         // var movieTitle = document.createTextNode(`${movie.Title}`);
@@ -24,7 +24,7 @@ function addMoviesToDom(movies) {
     });
 };
 
-// verwijderen vam movies van de dom
+
 const removeMovieFromDom = () => {
     const movies = movieNav.childNodes.length;
     for (let i = 0; i < movies; i++) {
@@ -32,48 +32,48 @@ const removeMovieFromDom = () => {
     }
 };
 
-// oppakken van de buttons 
+
 function addEventListener() {
     const filmfilter = document.querySelectorAll('input[type=radio][name="movie"]');
     filmfilter.forEach(radio => radio.addEventListener('change', () => {
-        console.log("de " + radio.value + " button is ingedrukt")
+       // console.log("this " + radio.value + " button is pussed")
         handleOnChangeEvent(radio.value)
     }));
 }
 addEventListener()
 
-//fiteren van nieuwe films 
+
 const newMovie = movies.filter((movie) => movie.Year >= 2014);
 
-// filteren van filmtype
+
 const filterMovies = (nameInMovieTitle) => {
     const filterMovies = movies.filter((movie) => movie.Title.includes(nameInMovieTitle));
-    console.log(filterMovies)
+    //console.log(filterMovies)
     return filterMovies;
 };
 
-// het activeren van de filter funtie
+
 function handleOnChangeEvent(event) {
     removeMovieFromDom();
     switch (event) {
         case 'newmovies':
-            console.log("newmovie");
+           // console.log("newmovie");
             addMoviesToDom(newMovie);
             break;
         case 'avenger':
-            console.log("avenger");
+            //console.log("avenger");
             addMoviesToDom(filterMovies("Avenger"));
             break;
         case 'xmen':
-            console.log("X-men");
+           // console.log("X-men");
             addMoviesToDom(filterMovies("X-Men"));
             break;
         case 'princess':
-             console.log("princess");
+            // console.log("princess");
             addMoviesToDom(filterMovies("Princess"));
             break;
         case 'batman':
-            console.log("batman");
+            //console.log("batman");
             addMoviesToDom(filterMovies("Batman"));
             break;
         default:
@@ -83,7 +83,7 @@ function handleOnChangeEvent(event) {
 }
 handleOnChangeEvent();
 
-// bonus item toegepast
+
 
 searchBar.addEventListener('keyup', (e) => {
     let searchTerm = e.target.value.toLowerCase();
